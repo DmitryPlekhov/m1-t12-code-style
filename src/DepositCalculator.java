5ceb14c6c3fd5bb94c15599f2e1ebfd316bdeb53
Dmitry <dimitriplehov@yandex.ru~>
2022-11-27 23:06:52 +0300

    second commit

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -18,9 +18,10 @@ public class DepositCalculator {
         return Math.round(value * scale) / scale;
     }
 
-    void do_important_job() {
+    void Calculate() {
         int period;
         int action;
+        double out = 0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:") ;
@@ -29,16 +30,15 @@ public class DepositCalculator {
         period = scanner.nextInt( );
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
-        double outDoubleVar = 0;
         if (action == 1) {
-            outDoubleVar = calculateSimplePercent(amount, 0.06, period);
+            out = calculateSimplePercent(amount, 0.06, period);
         } else if (action == 2) {
-            outDoubleVar = calculateComplexPercent(amount, 0.06, period);
+            out = calculateComplexPercent(amount, 0.06, period);
         }
-        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);
     }
 
     public static void main(String[] args) {
-        new DepositCalculator().do_important_job();
+        new DepositCalculator().Calculate();
     }
 }
